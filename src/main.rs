e8f77be3b4057def5199aad44e1bf91a4941db90
AntonCharnichenka <antt0n.chern1chenk0@gmail.com>
2024-06-02 19:22:02 +0200

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::io;   // :: means using associated function which is not a method in Rust
-use rand::Rng;   // Rng trait defines methods that random number generators implement, 
-// and this trait must be in scope for us to use those methods
+use rand::Rng; // Rng trait defines methods that random number generators implement, and this trait must be in scope for us to use those methods
 use std::cmp::Ordering;
+use std::io; // :: means using associated function which is not a method in Rust
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);  // range expression as an argument
+    let secret_number = rand::thread_rng().gen_range(1..=100); // range expression as an argument
 
     println!("The secret number is {secret_number}");
 
     loop {
         println!("Please input your guess:");
-        
+
         let mut guess = String::new();
 
         io::stdin()
@@ -30,18 +29,18 @@ fn main() {
             Ok(num) => num,
             Err(_) => {
                 println!("Please type a number!");
-                continue
-            },  // The underscore, _, is a catchall value
+                continue;
+            } // The underscore, _, is a catchall value
         }; // Shadowing values: when you want to convert a value from one type to another type.
 
-        match guess.cmp(&secret_number) { // The match expression ends after the first successful match
-            Ordering::Less => println!("Too small!"),  // arm of the match expression
+        match guess.cmp(&secret_number) {
+            // The match expression ends after the first successful match
+            Ordering::Less => println!("Too small!"), // arm of the match expression
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
-            },
-
+            }
         }
     }
-}   
+}
